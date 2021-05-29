ffcd4f5c0d1ac64ae3fff5f7fe2abdd79d05ff71
Paulína Palíková <paulipalik@gmail.com>
2021-05-29 12:48:51 +0200

    ukol-15-bez-keyprop

---
+++ src/Joke/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { jokes } from '../jokes';
+import './style.css';
+
+export const Joke = (props) => {
+  return (
+    <div>
+      {jokes.map((user) => {
+        const [upLikes, setUpLikes] = useState([user.likes]);
+        const [downLikes, setDownLikes] = useState([user.dislikes]);
+
+        return (
+          <div className="container">
+            <div className="joke">
+              <div className="joke__body">
+                <div className="joke__user">
+                  <img className="user-avatar" src={user.avatar} />
+                  <p className="user-name">{user.name}</p>
+                </div>
+
+                <p className="joke__text">{user.text}</p>
+              </div>
+              <div className="joke__likes">
+                <button
+                  id="btn-up"
+                  className="btn-like btn-like--up"
+                  onClick={() => setUpLikes((user.likes += 1))}
+                ></button>
+                <span id="likes-up" className="likes-count likes-count--up">
+                  {upLikes}
+                </span>
+                <button
+                  id="btn-down"
+                  className="btn-like btn-like--down"
+                  onClick={() => setDownLikes((user.dislikes += 1))}
+                ></button>
+                <span id="likes-down" className="likes-count likes-count--down">
+                  {downLikes}
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,51 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import { Joke } from './Joke/index';
 
 const App = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
+  // const [upLikes, setUpLikes] = useState(0);
+  // const [downLikes, setDownLikes] = useState(0);
 
   return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
+    <Joke />
+    // <div className="container">
+    //   <div className="joke">
+    //     <div className="joke__body">
+    //       <div className="joke__user">
+    //         <img
+    //           className="user-avatar"
+    //           src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+    //         />
+    //         <p className="user-name">Neroxx</p>
+    //       </div>
 
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setUpLikes(upLikes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {upLikes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDownLikes(downLikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {downLikes}
-          </span>
-        </div>
-      </div>
-    </div>
+    //       <p className="joke__text">
+    //         The secret service isn't allowed to yell "Get down!" anymore when
+    //         the president is about to be attacked. Now they have to yell
+    //         "Donald, duck!"
+    //       </p>
+    //     </div>
+    //     <div className="joke__likes">
+    //       <button
+    //         id="btn-up"
+    //         className="btn-like btn-like--up"
+    //         onClick={() => setUpLikes(upLikes + 1)}
+    //       ></button>
+    //       <span id="likes-up" className="likes-count likes-count--up">
+    //         {upLikes}
+    //       </span>
+    //       <button
+    //         id="btn-down"
+    //         className="btn-like btn-like--down"
+    //         onClick={() => setDownLikes(downLikes + 1)}
+    //       ></button>
+    //       <span id="likes-down" className="likes-count likes-count--down">
+    //         {downLikes}
+    //       </span>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
